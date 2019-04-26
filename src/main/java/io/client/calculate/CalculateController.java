b6c7bf5ef93109d91bf3fd81d845e434d4b892c4
pal_user <nchavez@gblsys.com>
2019-04-26 10:00:47 -0700

    editing controllers

--- src/main/java/io/client/calculate/CalculateController.java
+++ src/main/java/io/client/calculate/CalculateController.java
@@ -1,13 +1,20 @@
 package io.client.calculate;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculateController {
+  private String message;
+  public CalculateController(
+      @Value("${welcome.message}") String str
+  ) {
+    this.message = str;
+  }
   @GetMapping("/")
   public String sayHello() {
-    return "hi from calculator";
+    return message;
   }
 
 }
